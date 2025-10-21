d13765486a8a6041e31062b4f49fad7d192ee63e
davidzagyvaig <zagyvaigd@gmail.com>
2025-10-21 02:57:26 +0200

    Updated scraper and implemented hybrid search

--- chatbot.py
+++ chatbot.py
@@ -1,11 +1,15 @@
+from bs4 import BeautifulSoup
 import chainlit as cl
 from dotenv import load_dotenv
 import os
 
 from langchain_openai import OpenAIEmbeddings
 from langchain_qdrant import Qdrant
-from qdrant_client import QdrantClient
+from qdrant_client import QdrantClient, models
 from langchain_openai import ChatOpenAI
+from fastembed.sparse.bm25 import Bm25
+from fastembed.late_interaction import LateInteractionTextEmbedding
+import requests
 
 load_dotenv()
 URL = os.getenv("URL")
@@ -18,27 +22,66 @@ CHAT_MODEL = os.getenv("CHAT_MODEL")
 if not URL or not QDRANT_HOST or not QDRANT_PORT or not QDRANT_COLLECTION_NAME or not OPENAI_API_KEY or not MODEL or not CHAT_MODEL:
     raise Exception("Environment variables are not set")
 
-vector_db_client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
+client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
 embeddings = OpenAIEmbeddings(model=MODEL, api_key=OPENAI_API_KEY)
-vector_db = Qdrant(client=vector_db_client, collection_name=QDRANT_COLLECTION_NAME, embeddings=embeddings)
+vector_db = Qdrant(client=client, collection_name=QDRANT_COLLECTION_NAME, embeddings=embeddings)
 llm = ChatOpenAI(model=CHAT_MODEL, openai_api_key=OPENAI_API_KEY)
-history = []
+
+dense_embedding_model = OpenAIEmbeddings(model=MODEL, api_key=OPENAI_API_KEY)
+bm25_embedding_model = Bm25("Qdrant/bm25")
+late_interaction_embedding_model = LateInteractionTextEmbedding("colbert-ir/colbertv2.0")
+
+SYSTEM_PROMPT = {
+    "role": "system",
+    "content": "You are a helpful assistant that can answer questions about the Eötvös József Gimnázium weboldalának tartalma."
+}
+history: list[dict[str, str]] = []
 
 @cl.on_chat_start
 async def start():
     await cl.Message(
         content="Szia! Én az Eötvös József Gimnázium weboldalának chatbotja vagyok. Miben segíthetek?",
     ).send()
-    history.append({"role": "assistant", "content": "Szia! Én az Eötvös József Gimnázium weboldalának chatbotja vagyok. Miben segíthetek?"})
+    history.clear()
+    history.extend([SYSTEM_PROMPT, {"role": "assistant", "content": "Szia! Én az Eötvös József Gimnázium weboldalának chatbotja vagyok. Miben segíthetek?"}])
 
 @cl.on_message
 async def main(message: cl.Message):
     question = message.content
     history.append({"role": "user", "content": question})
-    query_results = vector_db.similarity_search(question)
+    query_results = retrieve_text(question)
     for result in query_results:
-        print(result.page_content)
-        history.append({"role": "assistant", "content": result.page_content})
+        history.append({"role": "assistant", "content": result.payload["text"]})
+        page_content = BeautifulSoup(requests.get(result.payload["url"], verify=False).text, "html.parser").get_text(separator="\n", strip=True)
+        history.append({"role": "assistant", "content": page_content})
+    await cl.Message(content="Fetched results from:\n" + "\n".join([result.payload["url"] for result in query_results])).send()
     response = llm.invoke(history)
     history.append({"role": "assistant", "content": response.content})
     await cl.Message(content=response.content).send()
+
+def retrieve_text(question: str):
+    dense_query = dense_embedding_model.embed_query(question)
+    sparse_query = next(bm25_embedding_model.query_embed(question))
+    late_query = next(late_interaction_embedding_model.query_embed(question))
+    results = client.query_points(
+        collection_name=QDRANT_COLLECTION_NAME,
+        prefetch=[
+            models.Prefetch(
+                prefetch=[
+                    models.Prefetch(
+                        query=models.SparseVector(**sparse_query.as_object()),
+                        using="bm25",
+                        limit=100,
+                    ),
+                ],
+                query=dense_query,
+                using="text-dense",
+                limit=25,
+            ),
+        ],
+        query=late_query,
+        using="colbertv2.0",
+        with_payload=True,
+        limit=5,
+    )
+    return results.points
--- extract.py
+++ extract.py
@@ -3,13 +3,15 @@ from bs4 import BeautifulSoup
 from dotenv import load_dotenv
 import os
 import argparse
+import uuid
 import urllib3
 urllib3.disable_warnings()
 
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from qdrant_client import QdrantClient, models
-from langchain_qdrant import Qdrant
 from langchain_openai import OpenAIEmbeddings
+from fastembed.sparse.bm25 import Bm25
+from fastembed.late_interaction import LateInteractionTextEmbedding
 
 load_dotenv()
 URL = os.getenv("URL")
@@ -21,32 +23,46 @@ MODEL = os.getenv("MODEL")
 if not URL or not QDRANT_HOST or not QDRANT_PORT or not QDRANT_COLLECTION_NAME or not OPENAI_API_KEY or not MODEL:
     raise Exception("Environment variables are not set")
 
-vector_db_client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
-try:
-    vector_db_client.delete_collection(collection_name=QDRANT_COLLECTION_NAME)
-    print(f"Collection '{QDRANT_COLLECTION_NAME}' deleted.")
-    vector_db_client.recreate_collection(
-        collection_name=QDRANT_COLLECTION_NAME,
-        vectors_config=models.VectorParams(size=1536, distance=models.Distance.COSINE),
-    )
-    print("Collection created successfully.")
-except Exception:
-    print(f"Collection '{QDRANT_COLLECTION_NAME}' does not exist, creating it.")
-    vector_db_client.recreate_collection(
-        collection_name=QDRANT_COLLECTION_NAME,
-        vectors_config=models.VectorParams(size=1536, distance=models.Distance.COSINE),
-    )
-    print("Collection created successfully.")
-embeddings = OpenAIEmbeddings(model=MODEL, api_key=OPENAI_API_KEY)
-vector_db = Qdrant(client=vector_db_client, collection_name=QDRANT_COLLECTION_NAME, embeddings=embeddings)
+dense_embedding_model = OpenAIEmbeddings(model=MODEL, api_key=OPENAI_API_KEY)
+bm25_embedding_model = Bm25("Qdrant/bm25")
+late_interaction_embedding_model = LateInteractionTextEmbedding("colbert-ir/colbertv2.0")
+
+client = QdrantClient(host=QDRANT_HOST, port=QDRANT_PORT)
+client.recreate_collection(
+    collection_name=QDRANT_COLLECTION_NAME,
+    vectors_config={
+        "text-dense": models.VectorParams(
+            size=1536,  # openai vector size
+            distance=models.Distance.COSINE,
+        ),
+        "colbertv2.0": models.VectorParams(
+            size=128,
+            distance=models.Distance.COSINE,
+            multivector_config=models.MultiVectorConfig(
+                comparator=models.MultiVectorComparator.MAX_SIM,
+            )
+        )
+    },
+    sparse_vectors_config={
+        "bm25": models.SparseVectorParams(
+            modifier=models.Modifier.IDF,
+        )
+    },
+)
 
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument("--start", type=int, required=False, default=0)
+    parser.add_argument("--search", type=str, required=False, default="basic")
     args = parser.parse_args()
     if not os.path.exists("links.txt"):
-        print("Links file not found, starting BFS")
-        bfs(URL)
+        print("Links file not found, starting search")
+        if args.search == "bfs":
+            bfs(URL)
+        elif args.search == "basic":
+            basic_search(URL)
+        else:
+            raise ValueError(f"Invalid search mode: {args.search}")
     print("Links obtained")
     embed_text(args.start)
     
@@ -73,6 +89,18 @@ def bfs(url: str) -> None:
         for link in visited:
             f.write(link + "\n")
 
+def basic_search(url: str) -> None:
+    links = set()
+    response = requests.get(url, verify=False)
+    soup = BeautifulSoup(response.text, "html.parser")
+    for link in soup.find_all("a"):
+        if link.get("href").startswith("https://www.ejg") and "wp-content" not in link.get("href"):
+            links.add(link.get("href"))
+    print(f"Found {len(links)} links")
+    with open("links.txt", "w") as f:
+        for link in links:
+            f.write(link + "\n")
+
 def embed_text(start: int) -> None:
     with open("links.txt", "r") as f:
         links = f.readlines()
@@ -86,9 +114,10 @@ def embed_text(start: int) -> None:
                 print(f"Failed to extract text from {link.strip()}")
                 continue
             chunks = chunk_text(text)
-            vector_db.add_texts(texts=chunks, metadatas=[{"url": link.strip()}] * len(chunks))
+            for chunk in chunks:
+                upload_points(chunk, link.strip())
         except Exception as e:
-            print(f"Failed to process link")
+            print(f"Failed to process link {link.strip()}: {e}")
     print("Text embedded")
 
 def extract_text(url: str) -> str | None:
@@ -97,12 +126,37 @@ def extract_text(url: str) -> str | None:
         return None
     soup = BeautifulSoup(response.text, "html.parser")
     text = soup.find("td", id="main-table-mid").get_text(separator="\n", strip=True).split("Naptár 2025-2026")[0].strip()
+    if len(text) < 300:
+        return None
     return text
 
 def chunk_text(text: str) -> list[str]:
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
     chunks = text_splitter.split_text(text)
     return chunks
+
+def upload_points(text: str, url: str) -> None:
+    id = str(uuid.uuid4())
+    dense_embedding = dense_embedding_model.embed_query(text)
+    bm25_embedding = list(bm25_embedding_model.embed(text))
+    late_interaction_embedding = list(late_interaction_embedding_model.embed(text))
+    client.upload_points(
+        collection_name=QDRANT_COLLECTION_NAME,
+        points=[
+            models.PointStruct(
+                id=id,
+                vector={
+                    "text-dense": dense_embedding,
+                    "bm25": bm25_embedding[0].as_object(),
+                    "colbertv2.0": late_interaction_embedding[0].tolist(),
+                },
+                payload={
+                    "url": url,
+                    "text": text,
+                }
+            )
+        ]
+    )
 
 if __name__ == "__main__":
     main()
